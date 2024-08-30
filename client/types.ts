5b72c4c3a153768904910c0fcecd52778e9a9015
LeonMendonca <leonmendonca619@gmail.com>
2024-08-30 11:24:41 +0530

    methods for requesting server

---
+++ client/types.ts
@@ -0,0 +1,23 @@
+//QUERY
+
+type Error = {
+  errorMessage: string;
+}
+
+//getAccounts
+type GetAccounts = {
+  userid?: string;
+  usernmae: string;
+  balance: string;
+  accountid: string;
+  wallet: number;
+}
+//getUserByAccountId
+
+//MUTATION
+
+//depositAmount
+//transactionAmount
+//withdrawAmount
+
+export {};
